73b52ccc03f328c1512e09a404ac86feda0300a2
William Ritchie <williamritchie@hotmail.ca>
2018-08-01 23:55:43 -0500

    Update cloudServer.sh

--- CloudServer/cloudServer.sh
+++ CloudServer/cloudServer.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-SERVICE_NAME=cloudServer
+SERVICE_NAME=strmServer
 PATH_TO_DIR=/home/pi/JavaPrograms/CloudServerApplication
-PATH_TO_CLASS=/home/pi/JavaPrograms/CloudServerApplication/CloudServer.class
-PATH_TO_OUT=/home/pi/JavaPrograms/CloudServerApplication/CloudServer.out
-PID_PATH_NAME=/tmp/cloudServer-pid
+PATH_TO_CLASS=/home/pi/JavaPrograms/CloudServerApplication/StrmServer.class
+PATH_TO_OUT=/home/pi/JavaPrograms/CloudServerApplication/StrmServer.out
+PID_PATH_NAME=/tmp/strmServer-pid
 #I need to remember the PID so the system can send the termination signal to it properly.
 case $1 in
     start)
@@ -11,7 +11,7 @@ case $1 in
         if [ ! -f $PID_PATH_NAME ]; then
 	    ROOTDIR=$(pwd)
             cd $PATH_TO_DIR
-            nohup java CloudServer > $PATH_TO_OUT 2>&1 &
+            nohup java StrmServer > $PATH_TO_OUT 2>&1 &
             cd $ROOTDIR
             echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
@@ -40,7 +40,7 @@ case $1 in
             echo "$SERVICE_NAME starting ..."
             ROOTDIR=$(pwd)
 	    cd $PATH_TO_DIR
-            nohup java CloudServer > $PATH_TO_OUT 2>&1 &
+            nohup java StrmServer > $PATH_TO_OUT 2>&1 &
             cd $ROOTDIR
             echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
