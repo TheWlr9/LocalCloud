71238587b2c7f5c8ae325ab3f8ebfaac0d3075e7
William Ritchie <williamritchie@hotmail.ca>
2018-09-05 19:27:45 -0500

    Update StrmServer.sh
    Added the beta requirements

--- CloudServer/StrmServer.sh
+++ CloudServer/StrmServer.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 SERVICE_NAME=strmServer
-PATH_TO_DIR=/home/pi/JavaPrograms/CloudServerApplication
-#PATH_TO_CLASS is unused, just there for reference.
-PATH_TO_CLASS=/home/pi/JavaPrograms/CloudServerApplication/StrmServer.class
-PATH_TO_OUT=/home/pi/JavaPrograms/CloudServerApplication/StrmServer.out
+PATH_TO_DIR=/home/pi/JavaPrograms/StrmServer
+PATH_TO_JAR=/home/pi/JavaPrograms/StrmServer/StrmServerBE.jar
+PATH_TO_OUT=/home/pi/JavaPrograms/StrmServer/etc/StrmServer.out
 PID_PATH_NAME=/tmp/strmServer-pid
 #I need to remember the PID so the system can send the termination signal to it properly.
 case $1 in
@@ -12,7 +11,7 @@ case $1 in
         if [ ! -f $PID_PATH_NAME ]; then
 	    ROOTDIR=$(pwd)
             cd $PATH_TO_DIR
-            sudo nohup java StrmServer > $PATH_TO_OUT 2>&1 &
+            sudo nohup java -jar -server StrmServerBE.jar myPassword docs/ > $PATH_TO_OUT 2>&1 &
             cd $ROOTDIR
             echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
@@ -27,6 +26,9 @@ case $1 in
             kill $PID;
             echo "$SERVICE_NAME stopped ..."
             rm $PID_PATH_NAME
+	    cd $PATH_TO_DIR
+	    cd etc/
+	    #echo "0" > log.cfg
         else
             echo "$SERVICE_NAME is not running ..."
         fi
@@ -38,10 +40,13 @@ case $1 in
             kill $PID;
             echo "$SERVICE_NAME stopped ...";
             rm $PID_PATH_NAME
+	    cd $PATH_TO_DIR
+	    cd etc/
+	    #echo "0" > log.cfg
             echo "$SERVICE_NAME starting ..."
             ROOTDIR=$(pwd)
 	    cd $PATH_TO_DIR
-            sudo nohup java StrmServer > $PATH_TO_OUT 2>&1 &
+            sudo nohup java -jar -server StrmServerBE.jar myPassword docs/ > $PATH_TO_OUT 2>&1 &
             cd $ROOTDIR
             echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
