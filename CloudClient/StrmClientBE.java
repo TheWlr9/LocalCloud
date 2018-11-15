4691ab11e8aee477661e050a60ac382a59342e7b
William Ritchie <williamritchie@hotmail.ca>
2018-11-14 22:27:03 -0600

    Update!
    Added refresh button + minor security fix

--- CloudClient/StrmClientBE.java
+++ CloudClient/StrmClientBE.java
@@ -3,7 +3,18 @@
  * @Title Will's cloud
  * @author William Leonardo Ritchie
  * 
- * @version 2.2.2
+ * @version 2.3.5
+ * 
+ * _2.3.5_
+ * 		~Added a refresh button! (FINALLY...)
+ * 		~Fixed security issue
+ * 
+ * _2.2.4_
+ * 		~Added strm.out output
+ * 
+ * _2.2.3_
+ * 		~Changed the layout of the code and tidied it up a little...
+ * 		~Added static window size!
  * 
  * _2.2.2_
  * 		~Added ENCRYPTION FINALLY
@@ -44,7 +55,7 @@
  * 			HUGE overheat in server CPU.
  * 
  * _1.5.1_
- * 		~Changed the IOException ERROR pop-up message to better explain the rror to the user.
+ * 		~Changed the IOException ERROR pop-up message to better explain the error to the user.
  * 		~Fixed a program crashing bug involving invisible links and switching pages
  * 
  * _1.5_
@@ -65,11 +76,13 @@ import javax.crypto.spec.SecretKeySpec;
 import graphics.StrmClientUI;
 
 public class StrmClientBE{
+  final static private String DEBUG= "-d";
+  final static public String OUT_FILE_PATH= "etc"+File.separator+"strm.out";
+  
   final static private int PORT= 42843;
   final static private int SLEEP= 250;
   
-  //Key is not actually this string of 0s in downloadable zip and jar. It is in here just for security reasons
-  final static private String KEY_SEED= "0000000000000000";
+  final static private String KEY_SEED= "****************";
   //KEY_SEED.length()==16 || KEY_SEED.length()==24 || KEY_SEED.length()==32
   //KEY_SEED must equal the same as KEY_SEED on server
   final static private Key key= new SecretKeySpec(KEY_SEED.getBytes(), "AES") ;
@@ -105,7 +118,8 @@ public class StrmClientBE{
   private static String password;
   private static byte[] byteArraySend;
   private static byte[] byteArrayReceive;
-  private static String[] cloudFilesNames;
+  private static String[] cloudFilesNames; //All of the files available
+  private static String[] pageFilesNames; //The files currently displayed on screen
   private static String[] tenderInfo; //This is used to store the IP address and the password
   
   private static File file= null;
@@ -114,14 +128,22 @@ public class StrmClientBE{
   
   private static File configFile= null;
   
+  private static PrintStream strmOut= null;
+  
   private static StrmClientUI graphics;
   
+  private static boolean debug;
   private static boolean escaping;
   
   
   
   
-  public static void main(String[] args){
+  public static void main(String[] args){    
+    if(args.length>=1 && args[0].equals(DEBUG)) 
+      debug= true;
+    else
+      debug= false;
+      
     	  escaping= false;
 	  graphics= new StrmClientUI(MAX_FILES_PER_PAGE);
 	  tenderInfo= new String[2];
@@ -161,7 +183,14 @@ public class StrmClientBE{
         if(graphics.isMousePressed()){
           double mouseX= graphics.mouseX();
           double mouseY= graphics.mouseY();
-          if(mouseX>StrmClientUI.SETTINGS_BUTTON_X-StrmClientUI.SETTINGS_BUTTON_WIDTH/2 && mouseX<StrmClientUI.SETTINGS_BUTTON_X+StrmClientUI.SETTINGS_BUTTON_WIDTH/2
+          if(mouseX>StrmClientUI.REFRESH_BUTTON_X-StrmClientUI.REFRESH_BUTTON_WIDTH/2 && mouseX<StrmClientUI.REFRESH_BUTTON_X+StrmClientUI.REFRESH_BUTTON_WIDTH/2
+		  && mouseY>StrmClientUI.REFRESH_BUTTON_Y-StrmClientUI.REFRESH_BUTTON_HEIGHT/2 && mouseY<StrmClientUI.REFRESH_BUTTON_Y+StrmClientUI.REFRESH_BUTTON_HEIGHT/2) {
+            updateCloudFilesNames();
+            if(pageNo>numOfPages)
+              pageNo--;
+            graphics.display(pageNo, numOfPages, pageFilesNames);
+          }
+          else if(mouseX>StrmClientUI.SETTINGS_BUTTON_X-StrmClientUI.SETTINGS_BUTTON_WIDTH/2 && mouseX<StrmClientUI.SETTINGS_BUTTON_X+StrmClientUI.SETTINGS_BUTTON_WIDTH/2
 		  && mouseY>StrmClientUI.SETTINGS_BUTTON_Y-StrmClientUI.SETTINGS_BUTTON_HEIGHT/2 && mouseY<StrmClientUI.SETTINGS_BUTTON_Y+StrmClientUI.SETTINGS_BUTTON_HEIGHT/2) {
             stringOutStream.println(SHUTDOWN);
             stringOutStream.flush();
@@ -185,12 +214,13 @@ public class StrmClientBE{
             
             if(mouseX<StrmClientUI.PAGE_L_X+StrmClientUI.PAGE_BUTTON_WIDTH/2) {
               //The prev button has been hit
-              System.out.println("LEFT");
               
               if(pageNo>1) {
         	pageNo--;
         	
-        	graphics.display(pageNo, numOfPages, cloudFilesNames);
+        	updatePageFilesNames();
+        	
+        	graphics.display(pageNo, numOfPages, pageFilesNames);
               }
             }
             else if(mouseX>StrmClientUI.PAGE_R_X-StrmClientUI.PAGE_BUTTON_WIDTH/2){
@@ -199,20 +229,23 @@ public class StrmClientBE{
               if(pageNo<numOfPages) {
         	pageNo++;
         	
-        	graphics.display(pageNo, numOfPages, cloudFilesNames);
+        	updatePageFilesNames();
+        	
+        	graphics.display(pageNo, numOfPages, pageFilesNames);
               }
             }
           }
           else if(mouseX>(StrmClientUI.FILES_BOX_X-StrmClientUI.FILES_BOX_WIDTH/2) && mouseX<(StrmClientUI.FILES_BOX_X+StrmClientUI.FILES_BOX_WIDTH/2)){
-            for(int i= 0; i<cloudFilesNames.length; i++){
+            for(int i= 0; i<pageFilesNames.length; i++){
+              //System.out.println(i);
               if(mouseY>(StrmClientUI.FILES_BOX_Y+i*StrmClientUI.TEXT_HEIGHT*StrmClientUI.FILES_BOX_SPACING_MULTIPLIER-StrmClientUI.TEXT_HEIGHT/2) && mouseY<(StrmClientUI.FILES_BOX_Y+i*StrmClientUI.TEXT_HEIGHT*StrmClientUI.FILES_BOX_SPACING_MULTIPLIER+StrmClientUI.TEXT_HEIGHT/2)){
-                System.out.println("File chosen: "+cloudFilesNames[i+(MAX_FILES_PER_PAGE*(pageNo-1))]);
+                System.out.println("File chosen: "+pageFilesNames[i]);
                 
                 Thread.sleep(200);
                 
-                graphics.saveFilePopup(cloudFilesNames[i+(MAX_FILES_PER_PAGE*(pageNo-1))]);
+                graphics.saveFilePopup(pageFilesNames[i]);
                 String savingName= graphics.getSavingName();
-                String fileToSave= cloudFilesNames[i+(MAX_FILES_PER_PAGE*(pageNo-1))];
+                String fileToSave= pageFilesNames[i];
                 String savingDirectory= graphics.getSavingDirectory();
                 
                 if(savingName!=null){
@@ -220,21 +253,21 @@ public class StrmClientBE{
                     
                     //Ask to see whether the user wants to delete the file from the cloud now
                     if(graphics.popupDeleteFileConfirmation())
-                  	  delete(cloudFilesNames[i+(MAX_FILES_PER_PAGE*(pageNo-1))]);
+                  	  delete(pageFilesNames[i]);
                     
                     //Get the new number of files
                     stringOutStream.println(NUM_FILES_REQ);
                     stringOutStream.flush();
                     cloudFilesNames= new String[Integer.parseInt(stringInStream.readLine())];
                     updateCloudFilesNames();
-                    if(cloudFilesNames.length%MAX_FILES_PER_PAGE==0)
+                    if((pageNo-1)*MAX_FILES_PER_PAGE>=cloudFilesNames.length)
                   	  pageNo--;
-                    graphics.display(pageNo, numOfPages, cloudFilesNames);
+                    graphics.display(pageNo, numOfPages, pageFilesNames);
                   }
                   graphics.clearLoading();
                 }
                 
-                //Thread.sleep(200);
+                break;
               }
             }
           }
@@ -262,7 +295,7 @@ public class StrmClientBE{
             	    pageNo= 1;
             	  if(numOfPages<=0)
             	    numOfPages= 1;
-            	  graphics.display(pageNo, numOfPages, cloudFilesNames);
+            	  graphics.display(pageNo, numOfPages, pageFilesNames);
             	  
             	  graphics.clearLoading();
               }
@@ -351,13 +384,6 @@ public class StrmClientBE{
     //Draw the empty loading buffer
     graphics.setupLoadingBar();
     
-	long waitTime= maxPingRecorded/2;
-    int bytesRead= 0;
-    int totalBytesRead= 0;
-    byte[] cipherText = null;
-    
-    boolean failed= false;
-    
     //Need to send the file name to the server
     stringOutStream.println(fileName);
     stringOutStream.flush();
@@ -368,31 +394,7 @@ public class StrmClientBE{
     
     Thread.sleep(SLEEP);
     
-    //Write the byte stream to the server
-    while((bytesRead= fileSend.read(byteArraySend))!=-1) {
-      try {
-	cipherText= encryptCipher.doFinal(byteArraySend,0,bytesRead);
-      } catch (Exception e) {
-	System.err.println("Error in encrypting the file");
-	e.printStackTrace();
-      }
-      
-      //Write the ciphertext to outStream (OutputStream)
-      outStream.write(cipherText);
-      //outStream.write(byteArray,0,bytesRead);
-      
-      if(!stringInStream.readLine().equals(SUCCESS_MSG)) {
-	failed= true;
-	break;
-      }
-      totalBytesRead+= bytesRead;
-      
-      graphics.updateLoadingBar((double)(totalBytesRead), (double)(file.length()));
-    }
-    
-    outStream.flush();
-    
-    if(!failed)
+    if(sendEncryptedBytes()) //This calls the method to send the file
     	System.out.println("Success!");
     else {
       System.err.println("Error, packet loss.");
@@ -421,6 +423,81 @@ public class StrmClientBE{
     stringOutStream.println(remoteFileName);
     stringOutStream.flush();
     
+    receiveDecryptedBytes(); //This receives the encrypted bytes from the connection
+  }
+  private static boolean download(String remoteFileName, String localPath, String localFileName) throws SecurityException, IOException, InterruptedException {
+    graphics.load();
+    
+    //Send download request to the server
+    stringOutStream.println(DOWNLOAD); //CONSTANT
+    
+    try {
+      receiveFile(remoteFileName, localPath, localFileName);
+      
+      fileReceive.close(); //Close the file stream
+      
+      return true; //Return successful
+    }
+    catch(SocketTimeoutException ste) {
+      System.err.println("Error, packet loss.");
+      
+      serverSocket.setSoTimeout(0); //Reset the socket timeout
+      
+      stringOutStream.println(ERROR_MSG);
+      stringOutStream.flush();
+      
+      if(fileReceive!=null)
+	fileReceive.close();
+      file.delete();
+      
+      if(graphics.popupPacketLossRetry()) {
+	graphics.clearLoading();
+	return download(remoteFileName, localPath, localFileName);
+      }
+      else
+	return false; //Return unsuccessful
+    }
+  }
+  
+  /**
+   * This method sends the bytes stored in byteArraySend, encrypted over the connection.
+   * @return Whether or not the method was successful
+   * @throws IOException If there is a problem with reading the file.
+   */
+  private static boolean sendEncryptedBytes() throws IOException {
+    int bytesRead;
+    int totalBytesRead= 0;
+    byte[] cipherText= null;
+  //Write the byte stream to the server
+    while((bytesRead= fileSend.read(byteArraySend))!=-1) {
+      try {
+	cipherText= encryptCipher.doFinal(byteArraySend,0,bytesRead);
+      } catch (Exception e) {
+	System.err.println("Error in encrypting the file");
+	e.printStackTrace();
+      }
+      
+      //Write the ciphertext to outStream (OutputStream)
+      outStream.write(cipherText);
+      //outStream.write(byteArray,0,bytesRead);
+      
+      if(!stringInStream.readLine().equals(SUCCESS_MSG))
+	return false;
+      totalBytesRead+= bytesRead;
+      
+      graphics.updateLoadingBar((double)(totalBytesRead), (double)(file.length()));
+    }
+    
+    outStream.flush();
+    
+    return true;
+  }
+  /**
+   * This method writes the decrypted byteArrayReceive array, into fileReceive, into file.
+   * @throws NumberFormatException When the size of the file is larger than signed int MAX SIZE
+   * @throws IOException When the file no longer exists for some odd reason.
+   */
+  private static void receiveDecryptedBytes() throws NumberFormatException, IOException {
     long sizeOfFile= Long.parseLong(stringInStream.readLine());
     long totalBytesRead= 0;
     //Write the byte stream to the file
@@ -462,39 +539,6 @@ public class StrmClientBE{
     serverSocket.setSoTimeout(0);
     
     System.out.println("Download complete!");
-  }
-  private static boolean download(String remoteFileName, String localPath, String localFileName) throws SecurityException, IOException, InterruptedException {
-    graphics.load();
-    
-    //Send download request to the server
-    stringOutStream.println(DOWNLOAD); //CONSTANT
-    
-    try {
-      receiveFile(remoteFileName, localPath, localFileName);
-      
-      fileReceive.close(); //Close the file stream
-      
-      return true; //Return successful
-    }
-    catch(SocketTimeoutException ste) {
-      System.err.println("Error, packet loss.");
-      
-      serverSocket.setSoTimeout(0); //Reset the socket timeout
-      
-      stringOutStream.println(ERROR_MSG);
-      stringOutStream.flush();
-      
-      if(fileReceive!=null)
-	fileReceive.close();
-      file.delete();
-      
-      if(graphics.popupPacketLossRetry()) {
-	graphics.clearLoading();
-	return download(remoteFileName, localPath, localFileName);
-      }
-      else
-	return false; //Return unsuccessful
-    }
   }
   
   public static void delete(String file) {
@@ -540,6 +584,17 @@ public class StrmClientBE{
     }
     
     numOfPages= (int)Math.ceil((double)cloudFilesNames.length/(double)MAX_FILES_PER_PAGE);
+   
+    updatePageFilesNames();
+  }
+  private static void updatePageFilesNames() {
+    pageFilesNames= new String[Math.min(MAX_FILES_PER_PAGE, cloudFilesNames.length-((pageNo-1)*MAX_FILES_PER_PAGE))];
+    short j= 0;
+    for(int i= (pageNo-1)*MAX_FILES_PER_PAGE; i<cloudFilesNames.length && i<MAX_FILES_PER_PAGE*pageNo; i++){ //The second condition is: i<(pageNo-1)*MAX_FILES_PER_PAGE+MAX_FILES_PER_PAGE
+      pageFilesNames[j]= cloudFilesNames[i]; //Deep copy
+      
+      j++; //Increment the second pointer in the page of files array
+    }
   }
   
   private static void startService() throws NumberFormatException, IOException  {
@@ -561,7 +616,11 @@ public class StrmClientBE{
     
     updateCloudFilesNames();
     
-    graphics.display(pageNo, numOfPages, cloudFilesNames); //Starting page #
+    //A check, in case the # of files has changed since last service initiation and pageNo!=1
+    if((pageNo-1)*MAX_FILES_PER_PAGE>=cloudFilesNames.length)
+      pageNo--;
+    
+    graphics.display(pageNo, numOfPages, pageFilesNames); //Starting page #
   }
   
   private static void openSettings() throws InterruptedException {
@@ -649,6 +708,15 @@ public class StrmClientBE{
   }
   
   private static void connect() throws IOException, PasswordException{
+    if(!debug) {
+      try {
+	strmOut= new PrintStream(new FileOutputStream(OUT_FILE_PATH), true);
+	System.setOut(strmOut);
+      } catch (FileNotFoundException e) {
+	System.err.println("Output file not found; using default System.out");
+      }
+    }
+    
     if(password.toLowerCase().equals("back"))
       throw new IOException();
     
@@ -680,6 +748,9 @@ public class StrmClientBE{
   
   private static void disconnect() {
     try{
+      if(strmOut!=null)
+	strmOut.close();
+      
       if(stringInStream!=null)
 	stringInStream.close();
       if(inStream!=null)
